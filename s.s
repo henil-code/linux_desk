6d5c4f34f9a6fe1c03957945fe6dd1edc7868be5
HENILCODES <prajapatihenil1112@gmail.com>
2022-07-26 08:14:44 +0530

    key sort

--- s.s
+++
@@ -1,8 +0,0 @@
-Emp_number |Emp_name | Emp_Salary 
- 101       |aman   |  5525555	
- 102       |henil    | 5000000    	
- 103       |ansh     | 3000000    	
- 104       |kevin    | 4000000  
-
-s
-s
